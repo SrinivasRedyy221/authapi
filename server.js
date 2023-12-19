2e4d433c572c2d266d762df178988b47905bc9eb
Srinivas <piserla.srinivas@gmail.com>
2023-12-19 18:55:31 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-import User from './models/User';
+import User from './models/User.js';
 
 dotenv.config();
 
