b19eb9a9a3605c2c83b65ab2bba54cf650c17cd8
Srinivas <piserla.srinivas@gmail.com>
2023-12-19 17:46:16 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -8,4 +9,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
